fea6613edb746d741d01cb4cf99a6f73d971968a
alegnz <gnzalejandro@gmail.com>
2019-12-28 17:08:41 -0300

    Completed module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,11 +3,20 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = { incorrectAnswer: false };
+    }
+
     handleClick(buttonText) {
         // Si el botón clickeado es el de la respuesta correcta,
         // se muestra la siguiente pregunta
         if (this.props.quiz_question.answer === buttonText) {
+            this.setState({ incorrectAnswer: false });
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState({ incorrectAnswer: true });
         }
     }
 
@@ -24,6 +33,8 @@ class QuizQuestion extends Component {
                         )}
                     </ul>
                 </section>
+
+                { this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null }
             </main>
         );
     }
